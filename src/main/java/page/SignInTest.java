418a9726b0971c6bbefdd4b4f565ddbd927fb326
Shubham <bansalshubham257@gmail.com>
2018-05-12 18:30:04 +0530

    Switching to frame modal_window to handle pop-up and click on signIn

--- src/main/java/page/SignInTest.java
+++ src/main/java/page/SignInTest.java
@@ -26,7 +26,7 @@ public class SignInTest {
 
         driver.findElement(By.linkText("Your trips")).click();
         driver.findElement(By.id("SignIn")).click();
-
+        driver.switchTo().frame("modal_window");
         driver.findElement(By.id("signInButton")).click();
 
         String errors1 = driver.findElement(By.id("errors1")).getText();
